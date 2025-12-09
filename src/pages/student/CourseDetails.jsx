3b4df5beabab73ff5053b71bb569c977da391dc2
manudeep <manudeep1000@gmail.com>
2025-12-09 23:57:15 +0530

    updated lecture section

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -3,10 +3,12 @@ import { useParams } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
 import { assets } from '../../assets/assets'
+import humanizeDuration from 'humanize-duration'
 const CourseDetails = () => {
   const {id}=useParams()
   const {allCourses,calculateRating,calculateChapterTime,calculateCourseDuration,calculateNoOfLectures}=useContext(AppContext)
   const [courseData,setCourseData]=useState(null);
+  const [openSection,setopenSection]=useState({});
   const fetchCourseData= async ()=>{
     const findCourse= allCourses.find(course=>course._id==id)
     setCourseData(findCourse)
@@ -14,6 +16,17 @@ const CourseDetails = () => {
   useEffect(()=>{
     fetchCourseData();
   },[])
+
+  const toggleSection=(index)=>{
+    setopenSection((prev)=>(
+      {...prev,
+        [index]:!prev[index]
+      }
+    )
+
+    )
+  }
+
   return courseData ? (
     <>
     <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start
@@ -38,24 +51,54 @@ const CourseDetails = () => {
         <p className='text-gray-500'>({courseData.courseRatings.length}{courseData.courseRatings>1 ? ' ratings':' rating'})</p>
         <p>({courseData.enrolledStudents.length} {courseData.enrolledStudents.length>1 ? 'students':'student'})</p>
         </div>
-        <p className='text-sm'>Course by <span className='text-blue-600 underline'>Manudeep</span></p>
+        <p className='text-sm'>Course by <span className='text-blue-600 underline'>Manudeep</span></p> 
+
+        {/* course structure section */}
         <div className='pt-8 text-gray-800'>
              <h2 className='text-xl font-semibold'>Course Structure</h2>
 
+             
              <div className='pt-5'>
               {courseData.courseContent.map((chapter,index)=>(
-                <div key={index} className='border border-gray-300 bg-gray-50 mb-2 rounded'>
+                <div key={index} className='border border-gray-300 bg-white  rounded'>
+
+                  {/* chapter section */}
+
                   <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
                     <div className='flex items-center gap-2'>
                         <img src={assets.down_arrow_icon} alt="arrow_icon" />
                         <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                     </div>
-                    <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures-{calculateChapterTime(chapter)}</p>
+                    <p className='text-sm md:text-base'>{chapter.chapterContent.length} lectures-{calculateChapterTime(chapter)}</p>
+                  </div>
+
+                  {/* lecture section */}
+
+                  <div className='overflow-hidden transition-all duration-300 max-h-96 '>
+                    <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
+                      {chapter.chapterContent.map((lecture,i)=>(
+                        <li key={index} className='flex items-start gap-2 py-2'>
+                          
+                          <img src={assets.play_icon} alt="playicon" className='mt-1' />
+                          
+                          <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-base'>
+                            <p>{lecture.lectureTitle}</p>
+                          <div className='flex gap-2'>
+                            {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                          <p>{humanizeDuration(lecture.lectureDuration*60*1000,{units:["h","m"]})}</p>
+
+                          </div>
+                         </div>
+
+                        </li>
+                      ))}
+                    </ul>
                   </div>
                 </div>
               ))}
              </div>
         </div>
+        
       </div>
       {/* rightcolumn */}
       <div></div>
