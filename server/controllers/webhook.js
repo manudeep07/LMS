7f4b96bfa70376cae5fabd111a677062d4e3bf23
manudeep <manudeep1000@gmail.com>
2026-01-29 03:42:26 +0530

    updated package.json

--- server/controllers/webhook.js
+++ server/controllers/webhook.js
@@ -1,11 +1,11 @@
 import { Webhook } from "svix";
-import User from "../models/User";
+import User from "../models/User.js";
 
 export const clerkWebhooks = async (req, res) => {
     try {
         const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        const evt=wh.verify(req.body, {
+        const evt = wh.verify(req.body, {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"],
@@ -50,7 +50,6 @@ export const clerkWebhooks = async (req, res) => {
 
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message:error.message });
+        res.status(500).json({ message: error.message });
     }
 };
-export {clerkWebhooks}
